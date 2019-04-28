32904d7a9a3f6fe054ccd7146827f1b5ee14caa9
Xiaocong Lau <12184795@qq.com>
2019-04-28 19:23:35 +0800

    [React-Router] Add router

---
+++ app/src/containers/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './styles.css';
+
+class Home extends React.Component {
+
+  render() {
+    return (
+      <div className={'title'}>
+        Home Page
+      </div>
+    );
+  }
+}
+
+export default Home;
